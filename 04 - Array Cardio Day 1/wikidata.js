297461e4a52cc3bb9430cad935bb5f616bbec09d
oliktva <oliktva@gmail.com>
2017-05-21 14:52:29 +0300

    done

---
+++ 04 - Array Cardio Day 1/wikidata.js
@@ -0,0 +1,133 @@
+var wikiData = [{
+            "pageid": 18339165,
+            "ns": 0,
+            "title": "Boulevards of Paris"
+        }, {
+            "pageid": 23524820,
+            "ns": 0,
+            "title": "City walls of Paris"
+        }, {
+            "pageid": 24570292,
+            "ns": 0,
+            "title": "Thiers wall"
+        }, {
+            "pageid": 23690618,
+            "ns": 0,
+            "title": "Wall of Charles V"
+        }, {
+            "pageid": 23629931,
+            "ns": 0,
+            "title": "Wall of Philip II Augustus"
+        }, {
+            "pageid": 18357662,
+            "ns": 0,
+            "title": "City gates of Paris"
+        }, {
+            "pageid": 4856477,
+            "ns": 0,
+            "title": "Haussmann's renovation of Paris"
+        }, {
+            "pageid": 18357472,
+            "ns": 0,
+            "title": "Boulevards of the Marshals"
+        }, {
+            "pageid": 18325129,
+            "ns": 0,
+            "title": "Boulevard Auguste-Blanqui"
+        }, {
+            "pageid": 40576013,
+            "ns": 0,
+            "title": "Boulevard Barb\u00e8s"
+        }, {
+            "pageid": 45130491,
+            "ns": 0,
+            "title": "Boulevard Beaumarchais"
+        }, {
+            "pageid": 29636994,
+            "ns": 0,
+            "title": "Boulevard de l'Amiral-Bruix"
+        }, {
+            "pageid": 21988904,
+            "ns": 0,
+            "title": "Boulevard des Capucines"
+        }, {
+            "pageid": 19088128,
+            "ns": 0,
+            "title": "Boulevard de la Chapelle"
+        }, {
+            "pageid": 22771680,
+            "ns": 0,
+            "title": "Boulevard de Clichy"
+        }, {
+            "pageid": 32433276,
+            "ns": 0,
+            "title": "Boulevard du Crime"
+        }, {
+            "pageid": 1888629,
+            "ns": 0,
+            "title": "Boulevard Haussmann"
+        }, {
+            "pageid": 3789348,
+            "ns": 0,
+            "title": "Boulevard de l'H\u00f4pital"
+        }, {
+            "pageid": 21451437,
+            "ns": 0,
+            "title": "Boulevard des Italiens"
+        }, {
+            "pageid": 21911534,
+            "ns": 0,
+            "title": "Boulevard de la Madeleine"
+        }, {
+            "pageid": 19087860,
+            "ns": 0,
+            "title": "Boulevard de Magenta"
+        }, {
+            "pageid": 14787162,
+            "ns": 0,
+            "title": "Boulevard Montmartre"
+        }, {
+            "pageid": 18295445,
+            "ns": 0,
+            "title": "Boulevard du Montparnasse"
+        }, {
+            "pageid": 18295110,
+            "ns": 0,
+            "title": "Boulevard Raspail"
+        }, {
+            "pageid": 22076425,
+            "ns": 0,
+            "title": "Boulevard Richard-Lenoir"
+        }, {
+            "pageid": 19088061,
+            "ns": 0,
+            "title": "Boulevard de Rochechouart"
+        }, {
+            "pageid": 2440023,
+            "ns": 0,
+            "title": "Boulevard Saint-Germain"
+        }, {
+            "pageid": 3670075,
+            "ns": 0,
+            "title": "Boulevard Saint-Michel"
+        }, {
+            "pageid": 18323079,
+            "ns": 0,
+            "title": "Boulevard de S\u00e9bastopol"
+        }, {
+            "pageid": 39750172,
+            "ns": 0,
+            "title": "Boulevard de Strasbourg"
+        }, {
+            "pageid": 15293925,
+            "ns": 0,
+            "title": "Boulevard du Temple"
+        }, {
+            "pageid": 45031764,
+            "ns": 0,
+            "title": "Boulevard Voltaire"
+        }, {
+            "pageid": 3483470,
+            "ns": 0,
+            "title": "Boulevard de la Zone"
+        }];
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 * 01 - JavaScript Drum Kit - [DONE](https://oliktva.github.io/js-practice/01%20-%20JavaScript%20Drum%20Kit/)
 * 02 - JS and CSS Clock	- [DONE](https://oliktva.github.io/js-practice/02%20-%20JS%20and%20CSS%20Clock/)
 * 03 - CSS Variables - [DONE](https://oliktva.github.io/js-practice/03%20-%20CSS%20Variables/)
-* 04 - Array Cardio Day 1
+* 04 - Array Cardio Day 1 - [DONE](https://oliktva.github.io/js-practice/04%20-%20Array%20Cardio%20Day%201/)
 * 05 - Flex Panel Gallery
 * 06 - Type Ahead
 * 07 - Array Cardio Day 2
